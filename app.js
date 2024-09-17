d2370a1ee55adda1d98b8475fc662f739a3710b9
varshabussa-1204 <150218699+varshabussa-1204@users.noreply.github.com>
2024-09-17 18:38:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const BASE_URL ="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns=document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("form button");
+const fromCurr=document.querySelector(".from select");
+const toCurr=document.querySelector(".to select");
+const msg=document.querySelector(".msg");
+
+const updateExchangeRate=async()=>{
+    let amount=document.querySelector(".amount input");
+    let amtval=amount.value;
+    console.log(amtval);
+    if(amtval==="" || amtval<1)
+    {
+        amtval=1;
+        amount.value="1";
+    }
+    const URL=`${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response=await fetch(URL);
+    let data=await response.json();
+    let rate=data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let finalamount=amtval*rate;
+    msg.innerText=`${amtval} ${fromCurr.value}=${finalamount} ${toCurr.value}`;
+}
+for (let select of dropdowns){
+    for (currcode in countryList){
+        let newOption=document.createElement("option");
+        newOption.innerText=currcode;
+        newOption.value=currcode;
+        if (select.name==="from" && currcode==="USD"){
+            newOption.selected="selected";
+        }
+        else if (select.name==="to" && currcode==="INR"){
+            newOption.selected="selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target);
+    });
+}
+const updateFlag=(element)=>{
+    let currcode=element.value;
+    let countrycode=countryList[currcode];
+    let newsrc=`https://flagsapi.com/${countrycode}/flat/64.png`;
+    let img=element.parentElement.querySelector("img");
+    img.src=newsrc;
+};
+
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load",()=>{
+    updateExchangeRate();
+})
